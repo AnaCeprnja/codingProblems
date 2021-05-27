09eaa18cde688f413dfdd58191f83bb13921940a
Ana Ceprnja <aceprnja@iun.edu>
2021-05-26 19:42:00 -0600

    solution added

--- problem.java
+++ problem.java
@@ -24,3 +24,5 @@ public boolean sleepIn(boolean weekday, boolean vacation) {
 firstLast6([1, 2, 6]) → true
 firstLast6([6, 1, 2, 3]) → true
 firstLast6([13, 6, 1, 2, 3]) → false */
+public boolean firstLast6(int[] nums)
+{	return (nums[0] == 6 || nums[nums.length-1] == 6);	}
