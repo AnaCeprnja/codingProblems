4f67fbd735fdfd7440f1e6a3bce6c868a9950bed
Ana Ceprnja <aceprnja@iun.edu>
2021-06-01 20:35:52 -0600

    10 problem

--- problem.py
+++ problem.py
@@ -106,3 +106,11 @@ print (evaluateExpressionTree(root))
 
   def near_hundred(n):
   return ((abs(100-n)<=10) or (abs(200-n) <=10))
+
+
+#Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+
+#makes10(9, 10) → True
+#makes10(9, 9) → False
+#makes10(1, 9) → True
