3515f94c30ca799a915b2fa13c569fd5f925415c
Ana Ceprnja <aceprnja@iun.edu>
2021-06-01 20:45:49 -0600

    clean up files

--- near_hundred.py
+++ near_hundred.py
@@ -12,13 +12,3 @@ def near_hundred(n):
   return ((abs(100-n)<=10) or (abs(200-n) <=10))
 
 
-#Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
-
-
-#makes10(9, 10) → True
-#makes10(9, 9) → False
-#makes10(1, 9) → True
-def makes10(a, b):
-  return (a == 10 or b == 10 or a+b == 10)
-print (makes10(8, 3))
-print (makes10(10, 3))
